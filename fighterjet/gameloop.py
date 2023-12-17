1eaf8ef2bf828f29f39914682b243789523ae133
Sam <scevans3452@yahoo.com>
2023-12-16 22:16:43 -0600

    music persists between states. options menu has volume control. added "Achievements" button to main menu, but no functionality yet

--- fighterjet/gameloop.py
+++ fighterjet/gameloop.py
@@ -1,7 +1,11 @@
-import pygame, random, sys, settings, player
+import pygame, random, sys, settings, player, settings
+from button import Button
 from explosion import Explosion
 from cloud import Cloud
 from pygame.locals import (RLEACCEL, K_ESCAPE, KEYDOWN, QUIT)
+
+def get_font(size):
+    return pygame.font.Font('freesansbold.ttf', size)
 
 class Game:
     def __init__(self):
@@ -10,23 +14,28 @@ class Game:
         pygame.display.set_caption('Fighter Pilot')
 
         # Variables
-        self.timer_duration      = 5000  # 5000 milliseconds = 5 seconds
-        self.background_color    = (135, 206, 250)
-        self.score_value         = 0
-        self.previous_high_score = 0
-        self.roll_breakdown      = [0,1,2,3] # breakdown of percentage of the roll from max potential
-        self.font                = pygame.font.Font('freesansbold.ttf', 32)
-        self.running             = True
+        self.timer_duration         = 5000  # 5000 milliseconds = 5 seconds
+        self.background_color       = (135, 206, 250)
+        self.score_value            = 0
+        self.previous_high_score    = 0
+        self.roll_breakdown         = [0,1,2,3] # breakdown of percentage of the roll from max potential
+        self.font                   = pygame.font.Font('freesansbold.ttf', 32)
+        self.running                = True
+        self.paused                 = False  # Initially, the game is not paused
 
         # Create custom events
         self.ADDENEMY = pygame.USEREVENT + 1
         pygame.time.set_timer(self.ADDENEMY, 250)
+
         self.ADDCLOUD = pygame.USEREVENT + 2
         pygame.time.set_timer(self.ADDCLOUD, 1000)
+
         self.INCREASE_SCORE = pygame.USEREVENT + 3
         pygame.time.set_timer(self.INCREASE_SCORE, 100)
+
         self.ADDPOWERUP = pygame.USEREVENT + 4
         self.set_interval(settings.MIN_INTERVAL, settings.MAX_INTERVAL, "powerup", self.ADDPOWERUP)
+
         self.TIMER_EVENT = pygame.USEREVENT + 5
 
         # Create groups to hold sprites
@@ -86,159 +95,189 @@ class Game:
     def run(self, state):
 
         pygame.init()
-        pygame.mixer.init()
-        # Load and play background music
-        # Sound source: http://ccmixter.org/files/Apoxode/59262
-        # License: https://creativecommons.org/licenses/by/3.0/
-        pygame.mixer.music.load("./assets/Apoxode_-_Electric_1.mp3")
-        pygame.mixer.music.play(loops=-1)
 
         # Start the main game loop
         while self.running:
+            if not self.paused:
+                pygame.mixer.music.set_volume(settings.ACTUAL_VOLUME)
+                # Process events, update game state, and draw elements
+                for event in pygame.event.get():
+                    # Did the user hit a key?
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            self.paused = not self.paused
+                            # self.running = False
+                    if event.type == QUIT:
+                        # Quit the game
+                        pygame.quit()
+                        sys.exit()
 
-            # Process events, update game state, and draw elements
-            for event in pygame.event.get():
-                # Did the user hit a key?
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
+                    # Add a new enemy
+                    elif event.type == self.ADDENEMY:
+                        # Create the new enemy and add it to sprite groups
+                        new_enemy = player.Enemy()
+                        self.enemies.add(new_enemy)
+                        self.all_sprites.add(new_enemy)
+
+                    # Add a new cloud
+                    elif event.type == self.ADDCLOUD:
+                        # Create the new cloud and add it to sprite groups
+                        new_cloud = Cloud()
+                        new_cloud.update_opacity(random.randint(160, 220))
+                        self.clouds.add(new_cloud)
+                        self.all_sprites.add(new_cloud)
+
+                    # score uptick
+                    elif event.type == self.INCREASE_SCORE:
+                        self.score_value += (1 * settings.MODIFIER)
+
+                    # Add a powerup
+                    elif event.type == self.ADDPOWERUP:
+                        self.power_list = [player.Shield(), player.Double_Points(), player.Ammo()]
+                        self.new_power = self.power_list[random.randint(0, (len(self.power_list)-1))]
+                        # print(f"{new_power}")
+                        self.powerups.add(self.new_power)
+                        self.all_sprites.add(self.new_power)
+                        # Reset the timer with a new random interval
+                        self.set_interval(settings.MIN_INTERVAL, settings.MAX_INTERVAL, "powerup", self.ADDPOWERUP)
+                    
+                    elif event.type == self.TIMER_EVENT:
+                        settings.MODIFIER = 1
+                        self.background_color = (135, 206, 250)  # Reset back to the original color
+
+                # Get the set of keys pressed and check for user input
+                pressed_keys = pygame.key.get_pressed()
+                self.player_obj.update(pressed_keys)
+                self.player_obj.bullets.update()
+
+                # Update the position of enemies and clouds
+                self.enemies.update()
+                self.powerups.update()
+                self.clouds.update()
+
+                # Fill the screen with sky blue
+                self.screen.fill(self.background_color)
+                self.previous_high_score = self.read_high_score('highscore.txt')
+                self.show_score(0, 0)
+                self.show_previous_high_score(0, 60)
+                self.show_defense(0, settings.SCREEN_HEIGHT-50)
+                self.show_ammo   (0, settings.SCREEN_HEIGHT-100)
+
+                self.all_sprites.add(self.player_obj.bullets)  # Add bullets to the group
+
+                # Draw all sprites
+                for entity in self.all_sprites:
+                    if isinstance(entity, Explosion):
+                        self.screen.blit(entity.image, entity.rect)
+                        entity.update()
+                    else:
+                        self.screen.blit(entity.surf, entity.rect)
+
+
+
+                #################
+                #   COLLISIONS  #
+                #################
+                for bullet in self.player_obj.bullets:
+                    self.screen.blit(bullet.surf, bullet.rect)
+                    self.enemy_hit_list = pygame.sprite.spritecollide(bullet, self.enemies, True)  # True removes the enemy on collision
+                    for enemy in self.enemy_hit_list:
+                        self.score_value += (50 * settings.MODIFIER)
+                        self.explosion = Explosion(enemy.rect.center) # Create an explosion at the enemy's position
+                        self.all_sprites.add(self.explosion)  # Add the explosion to all_sprites group
+                    if self.enemy_hit_list:  # Remove the bullet upon collision with an enemy
+                        bullet.kill()
+
+                # Enemy collision
+                collided_enemy = pygame.sprite.spritecollideany(self.player_obj, self.enemies)
+                if collided_enemy and not collided_enemy.collided:
+                    if self.player_obj.defense > 0:
+                        collided_enemy.kill()
+                        collided_enemy.collided = True
+                        self.player_obj.defense -= 1
+                        player.move_up_sound.stop()
+                        player.move_down_sound.stop()
+                        player.collision_sound.play()
+                        print(f"CLOSE CALL! Defense is now: {self.player_obj.defense}")
+                    else:
+                        self.player_obj.kill()
+                        player.move_up_sound.stop()
+                        player.move_down_sound.stop()
+                        player.collision_sound.play()
                         self.running = False
-                if event.type == QUIT:
-                    # Quit the game
-                    pygame.quit()
-                    sys.exit()
-
-                # Add a new enemy
-                elif event.type == self.ADDENEMY:
-                    # Create the new enemy and add it to sprite groups
-                    new_enemy = player.Enemy()
-                    self.enemies.add(new_enemy)
-                    self.all_sprites.add(new_enemy)
-
-                # Add a new cloud
-                elif event.type == self.ADDCLOUD:
-                    # Create the new cloud and add it to sprite groups
-                    new_cloud = Cloud()
-                    new_cloud.update_opacity(random.randint(160, 220))
-                    self.clouds.add(new_cloud)
-                    self.all_sprites.add(new_cloud)
-
-                # score uptick
-                elif event.type == self.INCREASE_SCORE:
-                    self.score_value += (1 * settings.MODIFIER)
-
-                # Add a powerup
-                elif event.type == self.ADDPOWERUP:
-                    self.power_list = [player.Shield(), player.Double_Points(), player.Ammo()]
-                    self.new_power = self.power_list[random.randint(0, (len(self.power_list)-1))]
-                    # print(f"{new_power}")
-                    self.powerups.add(self.new_power)
-                    self.all_sprites.add(self.new_power)
-                    # Reset the timer with a new random interval
-                    self.set_interval(settings.MIN_INTERVAL, settings.MAX_INTERVAL, "powerup", self.ADDPOWERUP)
-                
-                elif event.type == self.TIMER_EVENT:
-                    settings.MODIFIER = 1
-                    self.background_color = (135, 206, 250)  # Reset back to the original color
-
-            # Get the set of keys pressed and check for user input
-            pressed_keys = pygame.key.get_pressed()
-            self.player_obj.update(pressed_keys)
-            self.player_obj.bullets.update()
-
-            # Update the position of enemies and clouds
-            self.enemies.update()
-            self.powerups.update()
-            self.clouds.update()
-
-            # Fill the screen with sky blue
-            self.screen.fill(self.background_color)
-            self.previous_high_score = self.read_high_score('highscore.txt')
-            self.show_score(0, 0)
-            self.show_previous_high_score(0, 60)
-            self.show_defense(0, settings.SCREEN_HEIGHT-50)
-            self.show_ammo   (0, settings.SCREEN_HEIGHT-100)
-
-            self.all_sprites.add(self.player_obj.bullets)  # Add bullets to the group
-
-            # Draw all sprites
-            for entity in self.all_sprites:
-                if isinstance(entity, Explosion):
-                    self.screen.blit(entity.image, entity.rect)
-                    entity.update()
-                else:
-                    self.screen.blit(entity.surf, entity.rect)
 
 
-
-            #################
-            #   COLLISIONS  #
-            #################
-            for bullet in self.player_obj.bullets:
-                self.screen.blit(bullet.surf, bullet.rect)
-                self.enemy_hit_list = pygame.sprite.spritecollide(bullet, self.enemies, True)  # True removes the enemy on collision
-                for enemy in self.enemy_hit_list:
-                    self.score_value += (50 * settings.MODIFIER)
-                    self.explosion = Explosion(enemy.rect.center) # Create an explosion at the enemy's position
-                    self.all_sprites.add(self.explosion)  # Add the explosion to all_sprites group
-                if self.enemy_hit_list:  # Remove the bullet upon collision with an enemy
-                    bullet.kill()
-
-            # Enemy collision
-            collided_enemy = pygame.sprite.spritecollideany(self.player_obj, self.enemies)
-            if collided_enemy and not collided_enemy.collided:
-                if self.player_obj.defense > 0:
-                    collided_enemy.kill()
-                    collided_enemy.collided = True
-                    self.player_obj.defense -= 1
-                    player.move_up_sound.stop()
-                    player.move_down_sound.stop()
-                    player.collision_sound.play()
-                    print(f"CLOSE CALL! Defense is now: {self.player_obj.defense}")
-                else:
-                    self.player_obj.kill()
-                    player.move_up_sound.stop()
-                    player.move_down_sound.stop()
-                    player.collision_sound.play()
-                    self.running = False
-
-
-            # Powerup Collision
-            for powerup in self.powerups:
-                if pygame.sprite.collide_rect(self.player_obj, powerup):
-                    print(f"Power-up type: {powerup.powertype}")
-                    player.powerup_sound.stop()
-                    player.move_up_sound.stop()
-                    player.move_down_sound.stop()
-                    player.powerup_sound.play()
-                    
-                    if isinstance(powerup, player.Shield) and not powerup.collided:
-                        powerup.kill()
-                        self.player_obj.defense += 1
-                        powerup.collided = True
-                    
-                    elif isinstance(powerup, player.Ammo) and not powerup.collided:
-                        powerup.kill()
-                        powerup.collided = True
-                        self.player_obj.ammo += 1
+                # Powerup Collision
+                for powerup in self.powerups:
+                    if pygame.sprite.collide_rect(self.player_obj, powerup):
+                        print(f"Power-up type: {powerup.powertype}")
+                        player.powerup_sound.stop()
+                        player.move_up_sound.stop()
+                        player.move_down_sound.stop()
+                        player.powerup_sound.play()
                         
-                    elif isinstance(powerup, player.Double_Points) and not powerup.collided:
-                        powerup.kill()
-                        settings.MODIFIER = 2                               # Double points
-                        self.background_color = (225, 255, 0)                    # Gold
-                        pygame.time.set_timer(self.TIMER_EVENT, self.timer_duration)  # Start the timer
-                        powerup.collided = True
+                        if isinstance(powerup, player.Shield) and not powerup.collided:
+                            powerup.kill()
+                            self.player_obj.defense += 1
+                            powerup.collided = True
+                        
+                        elif isinstance(powerup, player.Ammo) and not powerup.collided:
+                            powerup.kill()
+                            powerup.collided = True
+                            self.player_obj.ammo += 1
+                            
+                        elif isinstance(powerup, player.Double_Points) and not powerup.collided:
+                            powerup.kill()
+                            settings.MODIFIER = 2                               # Double points
+                            self.background_color = (225, 255, 0)                    # Gold
+                            pygame.time.set_timer(self.TIMER_EVENT, self.timer_duration)  # Start the timer
+                            powerup.collided = True
 
-            # Cloud Collision
-            for cloud in self.clouds:
-                if cloud.rect.contains(self.player_obj):
-                    print("Player is completely covered by the cloud")
-                    # TODO:
-                    # ADD PLAYER SEEKING MISSILES
-                    # IF PLAYER IS HIDDEN BEHIND A CLOUD, THEN SEEKING MISSILE CAN'T TRACK PLAYER
+                # Cloud Collision
+                for cloud in self.clouds:
+                    if cloud.rect.contains(self.player_obj):
+                        print("Player is completely covered by the cloud")
+                        # TODO:
+                        # ADD PLAYER SEEKING MISSILES
+                        # IF PLAYER IS HIDDEN BEHIND A CLOUD, THEN SEEKING MISSILE CAN'T TRACK PLAYER
 
 
-            pygame.display.flip()
-            self.clock.tick(30) # Ensure program maintains a rate of 30 frames per second
+                pygame.display.flip()
+                self.clock.tick(30) # Ensure program maintains a rate of 30 frames per second
 
-        pygame.mixer.music.stop()
-        pygame.mixer.quit()
+            else:  # When paused, show the pause menu
+                pygame.mixer.music.set_volume(settings.ACTUAL_VOLUME*.3)
+                self.screen.fill((0, 0, 0))  # Clear the screen
+                MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+                # Render and display your pause menu elements
+                pause_text = get_font(50).render("Paused", True, (255, 255, 255))
+                pause_rect = pause_text.get_rect(center=(settings.SCREEN_WIDTH // 2, settings.SCREEN_HEIGHT // 2 - 50))
+                self.screen.blit(pause_text, pause_rect)
+
+                RESUME_BUTTON   = Button(image=None, pos=(settings.SCREEN_WIDTH // 2, settings.SCREEN_HEIGHT // 2 + 50), text_input="Resume", font=get_font(40), base_color="red", hovering_color="White")
+                QUIT_BUTTON     = Button(image=None, pos=(settings.SCREEN_WIDTH // 2, settings.SCREEN_HEIGHT // 2 + 100), text_input="Main Menu", font=get_font(40), base_color="red", hovering_color="White")
+                
+                for button in [RESUME_BUTTON, QUIT_BUTTON]:
+                    button.changeColor(MENU_MOUSE_POS)
+                    button.update(self.screen)
+
+                # Check for events in the pause menu
+                for event in pygame.event.get():
+                    if event.type == pygame.MOUSEBUTTONDOWN:
+                        if RESUME_BUTTON.hover(MENU_MOUSE_POS):
+                            self.paused = not self.paused  # Resume the game
+                        if QUIT_BUTTON.hover(MENU_MOUSE_POS):
+                            self.running = False
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            self.paused = not self.paused
+                    if event.type == QUIT:
+                        # Quit the game
+                        pygame.quit()
+                        sys.exit()
+
+                pygame.display.flip()
+
+        # pygame.mixer.music.stop()
+        # pygame.mixer.quit()
--- fighterjet/state_management.py
+++ fighterjet/state_management.py
@@ -1,5 +1,7 @@
-import pygame, sys
+import pygame, pygame_widgets, sys, settings
 
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 from gameloop import Game
 from button import Button
 from pygame.locals import (
@@ -8,7 +10,15 @@ from pygame.locals import (
         QUIT,
     )
 
-SCREEN = pygame.display.set_mode((1280, 720))
+SCREEN               = pygame.display.set_mode((1280, 720))
+center_offset_width  = int(SCREEN.get_width()/2)
+center_offset_height = int(SCREEN.get_height()/2)
+slider_width         = 500
+slider_height        = 2
+slider_text_width    = 50
+slider_output_width  = 30
+slider_offset_width  = (center_offset_width - int(slider_width/2))
+slider_offset_height = (center_offset_height - int(slider_height/2))
 
 def get_font(size):
     return pygame.font.Font('freesansbold.ttf', size)
@@ -16,30 +26,24 @@ def get_font(size):
 class MainMenu:
     def __init__(self):
         self.running = True
-        self.music_playing = False  # Add a flag to track if music is playing
+        # self.music_playing = False  # Add a flag to track if music is playing
         self.screen_width, self.screen_height = SCREEN.get_size()
-        self.menu_text = get_font(100).render("MAIN MENU", True, "#b68f40")
-        self.menu_rect = self.menu_text.get_rect(center=(640, 100))
+        self.menu_text = get_font(100).render("FIGHTER PILOT", True, "#b68f40")
+        self.menu_rect = self.menu_text.get_rect(center=(640, 150))
 
     def run(self, state_manager):
-        pygame.mixer.init()
-        # Load the background music only if it's not playing
-        if not self.music_playing:
-            pygame.mixer.music.load("./assets/main_menu.mp3")
-            pygame.mixer.music.play(loops=-1)
-            self.music_playing = True
-            
         while self.running:
             MENU_MOUSE_POS = pygame.mouse.get_pos()
             SCREEN.fill((0, 0, 0))  # Clear screen
             # Buttons
-            PLAY_BUTTON    = Button(image=None, pos=(640, 250), text_input="PLAY",    font=get_font(75), base_color="red", hovering_color="White")
-            OPTIONS_BUTTON = Button(image=None, pos=(640, 400), text_input="OPTIONS", font=get_font(75), base_color="red", hovering_color="White")
-            QUIT_BUTTON    = Button(image=None, pos=(640, 550), text_input="QUIT",    font=get_font(75), base_color="red", hovering_color="White")
+            PLAY_BUTTON         = Button(image=None, pos=(640, 300), text_input="PLAY",         font=get_font(70), base_color="red", hovering_color="White")
+            OPTIONS_BUTTON      = Button(image=None, pos=(640, 400), text_input="OPTIONS",      font=get_font(70), base_color="red", hovering_color="White")
+            ACHIEVEMENTS_BUTTON = Button(image=None, pos=(640, 500), text_input="ACHIEVEMENTS", font=get_font(70), base_color="red", hovering_color="White")
+            QUIT_BUTTON         = Button(image=None, pos=(640, 600), text_input="QUIT",         font=get_font(70), base_color="red", hovering_color="White")
             
             pygame.display.set_caption('Main Menu')
 
-            for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON, ACHIEVEMENTS_BUTTON]:
                 button.changeColor(MENU_MOUSE_POS)
                 button.update(SCREEN)
 
@@ -65,8 +69,8 @@ class MainMenu:
                         if button.hover(MENU_MOUSE_POS):
                             if button.text_input   == "PLAY":
                                 state_manager.start_game()
-                                self.music_playing = False
-                                state_manager.start_main_menu()
+                                # self.music_playing = False
+                                state_manager.start_main_menu() # don't want new instance, just got back to where we were
                             elif button.text_input == "OPTIONS":
                                 state_manager.change_state('options_menu')
                                 state_manager.run_current_state(state_manager)
@@ -83,32 +87,49 @@ class OptionsMenu:
     def __init__(self):
         self.running = True
         self.screen_width, self.screen_height = SCREEN.get_size()
-    
+
+        # VOLUME CONTROLS
+        self.volume_control = {
+            'volume_slider' : Slider(SCREEN, slider_offset_width, slider_offset_height, slider_width, slider_height, min=0, max=99, step=1, initial=99, handleColour=(0, 255, 255), colour=(255, 255, 255), handleRadius=10),
+            'description'   : TextBox(SCREEN, slider_offset_width - slider_text_width - 80, slider_offset_height+slider_height, slider_text_width, slider_height*7, fontSize=16, textColour=(255,222,222), colour=(0, 0, 0)),
+            'output'        : TextBox(SCREEN, slider_offset_width + slider_width + 20, slider_offset_height+slider_height, slider_output_width, slider_height*7, fontSize=16, textColour=(255,222,222), colour=(0, 0, 0))
+        }
+        self.volume_control['output'].disable()  # Act as label instead of textbox
+        self.volume_control['description'].setText("MUSIC VOLUME")  # Act as label instead of textbox
+
     def run(self, state_manager):
+        pygame.mixer.init()
         while self.running:
+            events = pygame.event.get()
             SCREEN.fill((0, 0, 0))  # Clear screen
             menu_text = get_font(100).render("OPTIONS", True, "#b68f40")
             menu_rect = menu_text.get_rect(center=(640, 100))
+
             MENU_MOUSE_POS = pygame.mouse.get_pos()
-            # Buttons
-            BACK_BUTTON    = Button(image=None, pos=(640, 250), text_input="BACK", font=get_font(75), base_color="red", hovering_color="White")
-
-            pygame.display.set_caption('Options Menu')
-
-            
+            BACK_BUTTON = Button(image=None, pos=(640, 250), text_input="BACK", font=get_font(75),
+                                 base_color="red", hovering_color="White")
             BACK_BUTTON.changeColor(MENU_MOUSE_POS)
             BACK_BUTTON.update(SCREEN)
 
             SCREEN.blit(menu_text, menu_rect)
 
+            # Draw and update the slider
+            # FOR LOOP IF THERE ARE MORE SLIDERS SETUP
+            self.volume_control['volume_slider'].listen(events)
+            self.volume_control['volume_slider'].draw()
+
+            slider_value = self.volume_control['volume_slider'].getValue()
+            settings.ACTUAL_VOLUME = slider_value / 99.0            # Convert the slider value to a volume between 0.0 and 1.0
+            pygame.mixer.music.set_volume(settings.ACTUAL_VOLUME)   # Set the mixer volume
+
             # Check for events
-            for event in pygame.event.get():
+            for event in events:
                 # Did the user hit a key?
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
-                        self.running = False
-                        pygame.quit()
-                        sys.exit()
+                        state_manager.start_main_menu()
+                        # pygame.quit()
+                        # sys.exit()
                 if event.type == QUIT:
                     self.running = False
                     # Quit the game
@@ -118,8 +139,11 @@ class OptionsMenu:
                     BACK_BUTTON.changeColor(MENU_MOUSE_POS)
                     BACK_BUTTON.update(SCREEN)
                     if BACK_BUTTON.hover(MENU_MOUSE_POS):
-                        state_manager.change_state('main_menu')
-                        state_manager.run_current_state(state_manager)
+                        state_manager.start_main_menu()
+                self.volume_control['output'].setText(self.volume_control['volume_slider'].getValue())
+
+            pygame_widgets.update(events)
+            pygame.display.update()
                         
             # Update and render elements specific to the options menu
             pygame.display.flip()
@@ -132,12 +156,19 @@ class StateManager:
             'gameplay': Game()
         }
         self.current_state = 'main_menu'
+        self.options_screen_active = False
 
     def change_state(self, new_state):
         self.current_state = new_state
 
     def run_current_state(self, state):
         self.states[self.current_state].run(state)
+
+    def play_game_music(self):
+        # Load and play background music
+        pygame.mixer.init()
+        pygame.mixer.music.load("./assets/game_music.mp3")
+        pygame.mixer.music.play(loops=-1)
 
     def start_game(self):
         # Set the current state to 'gameplay' and start the game loop
@@ -154,6 +185,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     state_manager = StateManager()
+    state_manager.play_game_music()
 
     while True:
         state_manager.run_current_state(state_manager)
